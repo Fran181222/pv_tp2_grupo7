c20715aa425a7cf46d572b59691262bcf8859775
Francisco <uepeie13@gmail.com>
2025-04-21 18:01:12 -0300

    agregando ejercicios 5 y 11

--- JS/ejercicio5.js
+++ JS/ejercicio5.js
@@ -1,3 +1,14 @@
 // 5. Declarar dos variables denominadas horas, minutos, asignar valores a cada una y
 // realizar la conversión para expresar esas horas y minutos en segundos. Mostrar por consola
 // las horas y minutos, luego el resultado de la conversión en segundos.
+// Declaración de variables
+let horas = 2;
+let minutos = 30;
+
+// Conversión a segundos
+let segundos = (horas * 3600) + (minutos * 60);
+
+// Mostrar resultados en consola
+console.log("Horas:", horas);
+console.log("Minutos:", minutos);
+console.log("Total en segundos:", segundos);
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Gabriel Ignacio Roel, Usuario: GabrielGIR
 Ruan Castiel Lindon Villanueva, Usuario: Ruancas
 Davide Arroyo, Usuario: arrdav
 Quispe Francisco Javier, Usuario: Fran181222
+
+11,17,23
+4,10,16,22
